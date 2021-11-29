744abf0501377e7288be364599d126fc3d52d960
Flowpk <fakepashookk@gmail.com>
2021-11-29 15:35:39 +0300

    initial commit

---
+++ src/shaders/fireflies/fragment1.glsl
@@ -0,0 +1,11 @@
+
+uniform float uColor;
+
+
+void main()
+{
+    float distanceToCenter = distance(gl_PointCoord, vec2(0.5));
+    float strength = 0.05 / distanceToCenter - 0.1;
+
+    gl_FragColor = vec4(uColor, 0 , 0, strength);
+}
